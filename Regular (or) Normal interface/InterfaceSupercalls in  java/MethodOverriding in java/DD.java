9693b5dc1706ecca38727d31ab95f2c15faefecb
harshadev-7 <2024cse.r102@svce.edu.in>
2025-10-19 16:48:23 +0530

    First commit

---
+++ Regular (or) Normal interface/InterfaceSupercalls in  java/MethodOverriding in java/DD.java
@@ -0,0 +1,36 @@
+interface A 
+{
+	default void display()
+	{
+		System.out.println("print a");
+	}
+}
+interface B
+{
+	default void display()
+	{	
+		System.out.println("print b");
+	}
+}
+class C implements A, B
+{
+	@Override
+	public void display()
+	{
+		System.out.println("print c");
+		A.super.display();
+		B.super.display();
+	}
+	void number(){
+	A.super.display();
+	}
+}
+public class DD
+{
+	public static void main(String[] args)
+	{
+		C c = new C();
+		c.display();
+		c.number();
+	}
+}
